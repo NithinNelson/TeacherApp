18a6a403461660044fd26f691cacddfa4d9b15a6
NithinNelson <nithinns2000@gmail.com>
2024-10-17 08:43:08 +0530

    ui

--- lib/View/MorePage/scan.dart
+++ lib/View/MorePage/scan.dart
@@ -1,111 +1,223 @@
-import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
-import 'package:teacherapp/View/CWidgets/TeacherAppPopUps.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:teacherapp/View/MorePage/qr_scanner_overlay_shape.dart';
 import 'package:teacherapp/View/MorePage/scanData.dart';
+import 'package:teacherapp/View/MorePage/scanner_error_widget.dart';
 
 import '../../Controller/api_controllers/qrController.dart';
-import '../../Models/api_models/qr_clinic_model.dart';
-import '../../Services/api_services.dart';
+import '../CWidgets/TeacherAppPopUps.dart';
 
 class QRViewExample extends StatefulWidget {
+  const QRViewExample({super.key});
+
   @override
-  State<StatefulWidget> createState() => _QRViewExampleState();
+  State<QRViewExample> createState() => _QRViewExampleState();
 }
 
 class _QRViewExampleState extends State<QRViewExample> {
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  QRViewController? controller;
-  String? qrText;
-
-  @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      controller?.pauseCamera();
-    }
-    controller?.resumeCamera();
-  }
+  final MobileScannerController controller = MobileScannerController(
+    formats: const [BarcodeFormat.qrCode],
+    // detectionSpeed: DetectionSpeed.noDuplicates,
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    final scanWindow = Rect.fromCenter(
+      center: MediaQuery.sizeOf(context).center(Offset.zero),
+      width: 200,
+      height: 200,
+    );
 
-
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 5,
-            child: QRView(
-              key: qrKey,
-              onQRViewCreated: (QRViewController qrController) {
-                _onQRViewCreated(qrController, context);
-              },
-              overlay: QrScannerOverlayShape(
-                borderColor: Colors.white,
-                borderRadius: 20,
-                borderLength: 30,
-                borderWidth: 12,
-                cutOutBottomOffset: 20,
-                cutOutSize: 300,
-                overlayColor: Colors.black.withOpacity(0.5),
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) {
+        Navigator.pop(context, true);
+      },
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        // appBar: AppBar(
+        //   title: const Text('Scanner with Overlay Example app'),
+        // ),
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Center(
+              child: MobileScanner(
+                fit: BoxFit.contain,
+                controller: controller,
+                scanWindow: scanWindow,
+                onDetect: (BarcodeCapture barcodes) async {
+                  print(
+                      ".qrData:.........................${barcodes.barcodes.first.displayValue}");
+                  if (barcodes.barcodes.first.displayValue != null) {
+                    print(
+                        "..........................${barcodes.barcodes.first.displayValue}");
+                    await Get.find<Qrcontroller>().fetchQrData(
+                        qrCode: barcodes.barcodes.first.displayValue
+                            .toString()
+                            .trim());
+                    // if(!mounted) return;
+                    if (Get.find<Qrcontroller>().studentqrdata.value.admnNo !=
+                        null) {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const Scandata()));
+                    } else {
+                      Navigator.of(context).pop();
+                      TeacherAppPopUps.submitFailed(
+                        title: "Failed",
+                        message: "Invalid QR Code",
+                        actionName: "Ok",
+                        iconData: Icons.error_outline,
+                        iconColor: Colors.red,
+                      );
+                    }
+                  }
+                },
+                errorBuilder: (context, error, child) {
+                  return ScannerErrorWidget(error: error);
+                },
+                // overlayBuilder: (context, constraints) {
+                //   return Padding(
+                //     padding: const EdgeInsets.all(16.0),
+                //     child: Align(
+                //       alignment: Alignment.bottomCenter,
+                //       child: StreamBuilder(
+                //         stream: controller.barcodes,
+                //         builder: (context, snapshot) {
+                //           final scannedBarcodes = snapshot.data?.barcodes ?? [];
+                //
+                //           if (scannedBarcodes.isEmpty) {
+                //             return const Text(
+                //               'Scan something!',
+                //               overflow: TextOverflow.fade,
+                //               style: TextStyle(color: Colors.white),
+                //             );
+                //           }
+                //
+                //           return Text(
+                //             scannedBarcodes.first.displayValue ?? 'No display value.',
+                //             overflow: TextOverflow.fade,
+                //             style: const TextStyle(color: Colors.white),
+                //           );
+                //         },
+                //       ),
+                //     ),
+                //   );
+                // },
               ),
-
             ),
-          ),
-          // Expanded(
-          //   flex: 1,
-          //   child: Center(
-          //     child: Text('Scan result: $qrText'),
-          //   ),
-          // )
-        ],
+            ValueListenableBuilder(
+              valueListenable: controller,
+              builder: (context, value, child) {
+                if (!value.isInitialized ||
+                    !value.isRunning ||
+                    value.error != null) {
+                  return const SizedBox();
+                }
+
+                return Container(
+                  decoration: ShapeDecoration(
+                    shape: QrScannerOverlayShape(
+                      borderColor: Colors.white,
+                      borderRadius: 20,
+                      borderLength: 30,
+                      borderWidth: 12,
+                      cutOutBottomOffset: 20,
+                      cutOutSize: 300,
+                      overlayColor: Colors.black.withOpacity(0.5),
+                    ),
+                  ),
+                );
+              },
+            ),
+            // Align(
+            //   alignment: Alignment.bottomCenter,
+            //   child: Padding(
+            //     padding: const EdgeInsets.all(16.0),
+            //     child: Row(
+            //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //       children: [
+            //         ToggleFlashlightButton(controller: controller),
+            //         SwitchCameraButton(controller: controller),
+            //       ],
+            //     ),
+            //   ),
+            // ),
+          ],
+        ),
       ),
     );
   }
 
-  void _onQRViewCreated(QRViewController controller, BuildContext mainContext) async {
-    print("....................................");
-
-    controller.scannedDataStream.listen((scanData) async {
-      print(".qrData:.........................${scanData.code}");
-      if (scanData.code != null) {
-        print("..........................$scanData");
-        await Get.find<Qrcontroller>().fetchQrData(qrCode: scanData.code.toString().trim());
-        // if(!mounted) return;
-        if(Get.find<Qrcontroller>().studentqrdata.value.admnNo != null) {
-          Navigator.of(mainContext).pushReplacement(MaterialPageRoute(builder: (context) => const Scandata()));
-
-        }
-        else {
-          Navigator.of(context).pop();
-
-          TeacherAppPopUps.submitFailed(
-              title: "Failed",
-              message:"Invalid QR Code",
-              actionName: "Ok",
-              iconData: Icons.error_outline,
-              iconColor: Colors.red);        }
-
-      }
-    });
+  @override
+  Future<void> dispose() async {
+    super.dispose();
+    await controller.dispose();
   }
+}
 
-  // void _onQRViewCreated(QRViewController controller) {
-  //   this.controller = controller;
-  //   controller.scannedDataStream.listen((scanData) {
-  //     setState(() {
-  //       qrText = scanData.code;
-  //     });
-  //   });
-  // }
+class ScannerOverlay extends CustomPainter {
+  const ScannerOverlay({
+    required this.scanWindow,
+    this.borderRadius = 12.0,
+  });
+
+  final Rect scanWindow;
+  final double borderRadius;
 
   @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
+  void paint(Canvas canvas, Size size) {
+    // TODO: use `Offset.zero & size` instead of Rect.largest
+    // we need to pass the size to the custom paint widget
+    final backgroundPath = Path()..addRect(Rect.largest);
+
+    final cutoutPath = Path()
+      ..addRRect(
+        RRect.fromRectAndCorners(
+          scanWindow,
+          topLeft: Radius.circular(borderRadius),
+          topRight: Radius.circular(borderRadius),
+          bottomLeft: Radius.circular(borderRadius),
+          bottomRight: Radius.circular(borderRadius),
+        ),
+      );
+
+    final backgroundPaint = Paint()
+      ..color = Colors.black.withOpacity(0.5)
+      ..style = PaintingStyle.fill
+      ..blendMode = BlendMode.dstOut;
+
+    final backgroundWithCutout = Path.combine(
+      PathOperation.difference,
+      backgroundPath,
+      cutoutPath,
+    );
+
+    final borderPaint = Paint()
+      ..color = Colors.orange
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4.0;
+
+    final borderRect = RRect.fromRectAndCorners(
+      scanWindow,
+      topLeft: Radius.circular(borderRadius),
+      topRight: Radius.circular(borderRadius),
+      bottomLeft: Radius.circular(borderRadius),
+      bottomRight: Radius.circular(borderRadius),
+    );
+
+    // First, draw the background,
+    // with a cutout area that is a bit larger than the scan window.
+    // Finally, draw the scan window itself.
+    canvas.drawPath(backgroundWithCutout, backgroundPaint);
+    canvas.drawRRect(borderRect, borderPaint);
+  }
+
+  @override
+  bool shouldRepaint(ScannerOverlay oldDelegate) {
+    return scanWindow != oldDelegate.scanWindow ||
+        borderRadius != oldDelegate.borderRadius;
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import emoji_picker_flutter
 import firebase_core
 import firebase_messaging
 import google_sign_in_ios
+import mobile_scanner
 import path_provider_foundation
 import record_darwin
 import shared_preferences_foundation
@@ -26,6 +27,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
